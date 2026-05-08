02a1294cf2180c5881ee9cbb33a0f55f674eb787
Alessandro Benelli <108730040+Tiltaments@users.noreply.github.com>
2026-05-08 16:48:50 +0200

    Add files via upload

---
+++ dati_generali.php
@@ -0,0 +1,9 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "alessandro.benelli.PHP-MySQL";
+
+$tab_clienti = "clienti";
+$tab_istruttori = "istruttori";
+$tab_acquisti = "acquisti";
